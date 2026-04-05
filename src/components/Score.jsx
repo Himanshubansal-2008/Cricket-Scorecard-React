5d5567cbe053fd9f4ee2ed522dd5364e6f40f64b
Himanshu <himanshubansal2608@gmail.com>
2026-04-06 00:19:43 +0530

    jsx and css added

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import './score.css';
 
 
 
@@ -12,85 +13,59 @@ function Score() {
 
     return (
         <div className="main-card">
-            <h1>Cricket Scorecard</h1>
-            <h2>Score: {score}</h2>
-            <h3>Balls: {balls}</h3>
-            <h3>Overs: {overs}</h3>
-            <h3>Wickets: {wicket}</h3>
-
+            <div className="display">
+                <h1>Cricket Scorecard</h1>
+                <h2>Score: {score}</h2>
+                <h3>Balls: {balls}</h3>
+                <h3>Overs: {overs}</h3>
+                <h3>Wickets: {wicket}</h3>
+            </div>
 
             <div className="score-btn">
-                <button onClick={()=> {
-                    setScore(score+1)
+                <button onClick={()=> {setScore(score+1)
                     if (balls==6){
                         setBalls(0)
                         setOvers(overs+1)
-
                     }else{
-                        setBalls(balls+1)
-                    }}}>1</button>
-                <button onClick={()=> {
-                    setScore(score+2)
+                        setBalls(balls+1)}}}>1</button>
+                <button onClick={()=> {setScore(score+2)
                     if (balls==6){
                         setBalls(0)
                         setOvers(overs+1)
-
                     }else{
-                        setBalls(balls+1)
-                    }}}>2</button>
-                <button onClick={()=> {
-                    setScore(score+3)
+                        setBalls(balls+1)}}}>2</button>
+                <button onClick={()=> {setScore(score+3)
                     if (balls==6){
                         setBalls(0)
                         setOvers(overs+1)
-
                     }else{
-                        setBalls(balls+1)
-                    }}}>3</button>
-                <button onClick={()=> {
-                    setScore(score+4)
+                        setBalls(balls+1)}}}>3</button>
+                <button onClick={()=> {setScore(score+4)
                     if (balls==6){
                         setBalls(0)
                         setOvers(overs+1)
-
                     }else{
-                        setBalls(balls+1)
-                    }}}>4</button>
-                <button onClick={()=> {
-                    setScore(score+5)
+                        setBalls(balls+1)}}}>4</button>
+                <button onClick={()=> {setScore(score+5)
                     if (balls==6){
                         setBalls(0)
                         setOvers(overs+1)
-
                     }else{
-                        setBalls(balls+1)
-                    }}}>5</button>
-                <button onClick={()=> {
-                    setScore(score+6)
+                        setBalls(balls+1)}}}>5</button>
+                <button onClick={()=> {setScore(score+6)
                     if (balls==6){
                         setBalls(0)
                         setOvers(overs+1)
-
                     }else{
-                        setBalls(balls+1)
-                    }}}>6</button>
-                <button onClick={() => {setWicket(wicket+1)}}>Add Wicket</button>
-                <button onClick={() => {
-                    setScore(0)
-                    setBalls(0)
-                    setWicket(0)
-                    setOvers(0)
-                }}>Restart Match</button>
-
-                <button onClick={() => {
-                    setScore(score+1)
-                }}>Wide Ball</button>
-
-                <button onClick={()=>{
-                    setScore(score+1)
-                }}>No Ball</button>
-
+                        setBalls(balls+1)}}}>6</button>
             </div>
+            <div className="special-btn">
+                <button className="wicket-btn" onClick={() => {setWicket(wicket+1)}}>Add Wicket</button>
+                <button className="restart-btn" onClick={() => {setScore(0);setBalls(0);setWicket(0);setOvers(0)}}>Restart Match</button>
+                <button className="wide-btn" onClick={() => {setScore(score+1)}}>Wide Ball</button>
+                <button className="noball-btn" onClick={()=>{setScore(score+1)}}>No Ball</button>
+            </div>
+
 
         </div>
     )
